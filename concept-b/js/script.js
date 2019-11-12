e296b4007410829d4582a3f357149c7d0e51635e
Ojus Doshi <yoshi22g@users.noreply.github.com>
2019-11-12 13:20:55 -0500

    tyring to update
    trying to replace

--- concept-b/js/script.js
+++ concept-b/js/script.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
   var controller = new ScrollMagic.Controller();
 
-  new ScrollMagic.Scene({
+  var scene1 = new ScrollMagic.Scene({
       triggerElement: "#trigger1",
-      triggerHook: .6, // show, when scrolled 40% into view
-      duration: "100%", // hide 10% before exiting view (80% + 10% from bottom)
+      triggerHook: .8, // show, when scrolled 40% into view
+      duration: "75%", // hide 10% before exiting view (80% + 10% from bottom)
       offset: 0 // move trigger to center of element
     })
-    .setClassToggle("#reveal1", "visible") // add class to reveal
+    .setClassToggle(".reveal1", "visible") // add class to reveal1
     //.addIndicators() // add indicators (requires plugin)
-    .addTo(controller);
+
+
+  var scene2 = new ScrollMagic.Scene({
+    triggerElement: "#trigger1",
+    triggerHook: .6, // show, when scrolled 20% into view
+    duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+    offset: 0 // move trigger to center of element
+  })
+    .setClassToggle(".reveal2", "visible") // add class to reveal2
+    //.addIndicators() // add indicators (requires plugin)
+
+    controller.addScene([
+      scene1,
+      scene2,
+    ]);
 });
